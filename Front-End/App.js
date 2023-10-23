89f59a2e81efe12aec0c4d68217f3a99be2e11f1
huise0ng <shinhuiseong07@gmail.com>
2023-10-23 13:25:50 +0900

    FIX :: 기본 세팅

--- Front-End/App.js
+++ Front-End/App.js
@@ -1,7 +1,17 @@
-function App() {
-    return(
-        <div>
-            <h1>기본세팅입니당</h1>
-        </div>
-    )
+import { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </>
+  );
 }
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
